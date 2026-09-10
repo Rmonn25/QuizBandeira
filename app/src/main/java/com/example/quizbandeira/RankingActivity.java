4954949093b7b8689c927fcff9a89bb3cd7747c0
Pedro Miguel Bezerra Da Silva <pedrobezerrasilva95@gmail.com>
2026-09-09 23:05:07 -0300

    Ranking
    Mexi nas telas do rankings

--- app/src/main/java/com/example/quizbandeira/RankingActivity.java
+++ app/src/main/java/com/example/quizbandeira/RankingActivity.java
@@ -23,8 +23,10 @@ package com.example.quizbandeira;
 // A pontuação será recebida da lógica do Quiz.
 // Não criar a lógica de pontuação nesta Activity.
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -32,6 +34,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RankingActivity extends AppCompatActivity {
+
+    private TextView tvNomeUsuario;
+    private TextView tvAcertos;
+    private Button btnResponderNovamente;
+    private Button btnTelaPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +50,6 @@ public class RankingActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
